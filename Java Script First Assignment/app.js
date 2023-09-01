4e66b854805ce766aee0ef8fd664c49ecad52d5a
Saad Ahmed <saad45257@gmail.com>
2023-09-01 20:28:33 +0500

    1st assignment

---
+++ Java Script First Assignment/app.js
@@ -0,0 +1,48 @@
+//Q no1
+var userName = 'Saad Ahmed'
+
+//Q no 2
+var myName = "Saad Ahmed"
+var age = 25 + " Years Old"
+
+// alert(myName)
+// alert(age)
+//Q no 3
+var message = "Hello World"
+// alert(message)
+
+//Q no 4 
+var bioData = "Salesman Of Artificial Jewellery Shop"
+// alert(bioData)
+
+//Q no 5 
+var text = "Pizza \nPizz \nPiz \nPi \nP"
+// alert(text)
+
+//Q no 6
+var myData = "I Will Be Software Developer in Pakistan, and married to Hafiza Aqsa with no kids"
+// alert(myData)
+
+//Q no 7
+var email = 'My email address is saad45257@gmail.com'
+// alert(email)
+
+//Q no 8
+var book = "A smarter way to learn JavaScript"
+// alert("I am learn a book of " + book)
+
+//Q no 9
+var exercise4 = "My name is Saad Ahmed" + " " + "I am 17 years old" + " " + "I study in SMIT"
+// alert(exercise4)
+
+//Q no 10
+document.write("I write HTML content in using Java Script")
+
+
+
+
+
+
+
+
+
