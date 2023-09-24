1da4c670d9a6ef4140a7aad4852dcc98d872b730
Saad Ahmed <saad45257@gmail.com>
2023-09-25 01:40:19 +0500

    Changes

--- Java Script 8th Assignment/app.js
+++ Java Script 8th Assignment/app.js
@@ -1,91 +1,79 @@
 //....................................Q1.............................................................
 
-// var num = prompt("Enter Number")
+var num = prompt("Enter Number")
 
-// console.log("Number: " + num)
-// console.log("Round off value: " + Math.round(num))
-// console.log("Floor value: " + Math.floor(num))
-// console.log("Ceil value: " + Math.ceil(num))
+console.log("Number: " + num)
+console.log("Round off value: " + Math.round(num))
+console.log("Floor value: " + Math.floor(num))
+console.log("Ceil value: " + Math.ceil(num))
 
 //....................................Q2.............................................................
 
-// var num = prompt("Enter Negative Number")
+var num = prompt("Enter Negative Number")
 
-// console.log("Number: " + num)
-// console.log("Round off value: " + Math.round(num))
-// console.log("Floor value: " + Math.floor(num))
-// console.log("Ceil value: " + Math.ceil(num))
+console.log("Number: " + num)
+console.log("Round off value: " + Math.round(num))
+console.log("Floor value: " + Math.floor(num))
+console.log("Ceil value: " + Math.ceil(num))
 
 //....................................Q3.............................................................
 
-// var num = prompt("Enter Positive floating Point Number")
+var num = prompt("Enter Positive floating Point Number")
 
-// console.log("Number: " + num)
-// console.log("Round off value: " + Math.round(num))
-// console.log("Floor value: " + Math.floor(num))
-// console.log("Ceil value: " + Math.ceil(num))
+console.log("Number: " + num)
+console.log("Round off value: " + Math.round(num))
+console.log("Floor value: " + Math.floor(num))
+console.log("Ceil value: " + Math.ceil(num))
 
 //....................................Q4.............................................................
 
-// var num = prompt("Enter Negative floating Point Number")
+var num = prompt("Enter Negative floating Point Number")
 
-// console.log("Number: " + num)
-// console.log("Round off value: " + Math.round(num))
-// console.log("Floor value: " + Math.floor(num))
-// console.log("Ceil value: " + Math.ceil(num))
+console.log("Number: " + num)
+console.log("Round off value: " + Math.round(num))
+console.log("Floor value: " + Math.floor(num))
+console.log("Ceil value: " + Math.ceil(num))
 
 //....................................Q5.............................................................
 //....................................Q6.............................................................
 
-// console.log("Random dice value:" + Math.ceil(Math.random() * 6))
-// console.log("Random dice value:" + Math.ceil(Math.random() * 6))
+console.log("Random dice value:" + Math.ceil(Math.random() * 6))
+console.log("Random dice value:" + Math.ceil(Math.random() * 6))
 
 //....................................Q7.............................................................
 
-// var coin = Math.ceil(Math.random()* 2)
+var coin = Math.ceil(Math.random()* 2)
 
-// if(coin == "2"){
-//     console.log("Heads")
-// }
-// else{
-//     console.log("Tails")
+if(coin == "2"){
+    console.log("Heads")
+}
+else{
+    console.log("Tails")
 // }
 
 //....................................Q8.............................................................
 
-// var coin = Math.ceil(Math.random()* 100)
+var coin = Math.ceil(Math.random()* 100)
 
-// console.log("Random Number Between 1 & 100: " + coin)
+console.log("Random Number Between 1 & 100: " + coin)
 
 //....................................Q9.............................................................
 
-// var weight = prompt("Enter Your Weight?.....")
+var weight = prompt("Enter Your Weight?.....")
 
 
 
-// console.log("The Weight Of User is " + weight + " Kilograms")
+console.log("The Weight Of User is " + weight + " Kilograms")
 
 //....................................Q10.............................................................
 
-// var secretNumber = 7
-// var userInput = prompt("Enter NUmber Between 1 to 10")
+var secretNumber = 7
+var userInput = prompt("Enter NUmber Between 1 to 10")
 
-// if(secretNumber == userInput){
-//     console.log("Conratulations You Are Win")
-// }
-// else{
-//     console.log("Try Again")
-// }
+if(secretNumber == userInput){
+    console.log("Conratulations You Are Win")
+}
+else{
+    console.log("Try Again")
+}
 
-//....................................Q11.............................................................
-
-
-//....................................Q12.............................................................
-//....................................Q13.............................................................
-//....................................Q14.............................................................
-//....................................Q15.............................................................
-//....................................Q16.............................................................
-//....................................Q17.............................................................
-//....................................Q18.............................................................
-//....................................Q19.............................................................
-//....................................Q20.............................................................
--- Java Script 9th Assignment/function.js
+++ Java Script 9th Assignment/function.js
@@ -2,231 +2,226 @@
 //............................Q1.......................................
 
 
-// function all(){
-//     alert("Hello World")""
-// }
+function all(){
+    alert("Hello World")""
+}
 
-// all()
+all()
 
 //............................Q2.......................................
 
-// function currentDate(){
-//     var date = new Date()
-//     console.log(date)
-// }
+function currentDate(){
+    var date = new Date()
+    console.log(date)
+}
 
-// currentDate()
+currentDate()
 //............................Q3.......................................
 
-// function greet (){
-//     var firstName = "Saad"
-//     var lastName = "Ahmed"
-//     alert(firstName + " " + lastName)
-// }
+function greet (){
+    var firstName = "Saad"
+    var lastName = "Ahmed"
+    alert(firstName + " " + lastName)
+}
 
-// greet()
+greet()
 //............................Q4.......................................
 
-// function sum(res){
-//     var firstNum = +prompt("Enter Sum Of First Number")
-//     var SecondNum = +prompt("Enter Sum Of Second Number")
-//     var result = firstNum + SecondNum
-//     return result
+function sum(res){
+    var firstNum = +prompt("Enter Sum Of First Number")
+    var SecondNum = +prompt("Enter Sum Of Second Number")
+    var result = firstNum + SecondNum
+    return result
 
-// }
+}
 
-// console.log(sum())
+console.log(sum())
 
 //............................Q5.......................................
 
-// function operator(num1,num2,opp){
-//     var opp = prompt("Enter Operation What do you want to perform")
-// switch (opp) {
+function operator(num1,num2,opp){
+    var opp = prompt("Enter Operation What do you want to perform")
+switch (opp) {
 
-//     case "+":
-//    return num1 + num2;
+    case "+":
+   return num1 + num2;
 
-//     case "-":
-//      return   num1 - num2;
+    case "-":
+     return   num1 - num2;
 
-//      case "*":
-//         return   num1 * num2;
+     case "*":
+        return   num1 * num2;
 
-//         case "/":
-//             return   num1 / num2;
+        case "/":
+            return   num1 / num2;
 
-//         default: return num1 + num2;
-//     }
+        default: return num1 + num2;
+    }
 
-// }
-// console.log(operator(5,7))
+}
+console.log(operator(5,7))
 
 //............................Q6.......................................
 
-// function square(a,b) {
+function square(a,b) {
 
-//     return Math.pow(a,b=2)
+    return Math.pow(a,b=2)
 
-// }
+}
 
-// square(8)
+square(8)
 
 //............................Q7.......................................
 
-// function factoral(n) {
-//     if (n == 0 || n == 1) {
-//         return 1
-//     }
-//     else {
-//        return n * factoral(n - 1)
-//     }
-// }
+function factoral(n) {
+    if (n == 0 || n == 1) {
+        return 1
+    }
+    else {
+       return n * factoral(n - 1)
+    }
+}
 
-// console.log(factoral(4))
+console.log(factoral(4))
 
 //............................Q8.......................................
 
-// function counting(){
-//     var firstNum = prompt("Enter First Number Start Of The Counting")
-//     var lastNum = prompt("Enter last Number End Of The Counting")
-//     for(var i = firstNum; i <= lastNum; i++){
-//         console.log(i)
-//     }
-// }
-// counting()
+function counting(){
+    var firstNum = prompt("Enter First Number Start Of The Counting")
+    var lastNum = prompt("Enter last Number End Of The Counting")
+    for(var i = firstNum; i <= lastNum; i++){
+        console.log(i)
+    }
+}
+counting()
 
 
 //............................Q9........................................
 
-// function calculateHypotenuse(){
-//     var base = prompt("Enter Base Value")
-//     var per = prompt("Enter Perpendicular Value")
+function calculateHypotenuse(){
+    var base = prompt("Enter Base Value")
+    var per = prompt("Enter Perpendicular Value")
 
 
-// function calculateSquare(value){
-//       var value = value * value
-//       return value
-// }
-//     var result = base + per
-//     return result
-// }
+function calculateSquare(value){
+      var value = value * value
+      return value
+}
+    var result = base + per
+    return result
+}
 
-// console.log(calculateHypotenuse())
+console.log(calculateHypotenuse())
 
 
 //............................Q10.......................................
 
-// function arr() {
-//     var array = []
-//     var length = prompt("Enter Variable Length")
-//     for (var i = 0; i < length; i++) {
-//     array.push(prompt("Enter Value"))
-// }
-//    return array
-// }
+function arr() {
+    var array = []
+    var length = prompt("Enter Variable Length")
+    for (var i = 0; i < length; i++) {
+    array.push(prompt("Enter Value"))
+}
+   return array
+}
 
-// document.write(arr())
+document.write(arr())
 
 //............................Q11.......................................
 
-// function largestNum(firstNum , SecondNum){
-//     if(firstNum > SecondNum){
-//         return firstNum
-//     }
-//     else{
-//         return SecondNum
-//     }
+function largestNum(firstNum , SecondNum){
+    if(firstNum > SecondNum){
+        return firstNum
+    }
+    else{
+        return SecondNum
+    }
    
-// }
+}
 
-// document.write(largestNum(30,50))
+document.write(largestNum(30,50))
 
 //............................Q12.......................................
 
-// function calculateArea(){
-//     var width = prompt("Enter Area Width")
-//     var height = prompt("Enter Area Height")
-//     var area = width * height
-//     return area
-// }
-// console.log(calculateArea())
+function calculateArea(){
+    var width = prompt("Enter Area Width")
+    var height = prompt("Enter Area Height")
+    var area = width * height
+    return area
+}
+console.log(calculateArea())
 
 //............................Q13.......................................
 
-// function sortedArray(){
-//     var arrLength = prompt("Enter Array Length")
-//     var array = []
-//     for(var i =0; i < arrLength; i++){
-//         array.push(prompt("Enter Index Of " + i))
-//     }
-//     array = array.sort();
-//     return array
-// }
+function sortedArray(){
+    var arrLength = prompt("Enter Array Length")
+    var array = []
+    for(var i =0; i < arrLength; i++){
+        array.push(prompt("Enter Index Of " + i))
+    }
+    array = array.sort();
+    return array
+}
 
-// document.write(sortedArray())
+document.write(sortedArray())
 
 //............................Q14.......................................
 
-// var array = [5,2,4,9,12]
-// var result = 0
-// function sum(){
-//    for(var i =0; i < array.length; i++){
-//     result += array[i]
-//    }
-//    return result
-// }
+var array = [5,2,4,9,12]
+var result = 0
+function sum(){
+   for(var i =0; i < array.length; i++){
+    result += array[i]
+   }
+   return result
+}
 
-// console.log(sum())
+console.log(sum())
 //............................Q15.......................................
 // //............................Q16.......................................
 
-// function power(){
-//     var num = prompt("Enter Your Number")
-//     return num * num * num
-// }
+function power(){
+    var num = prompt("Enter Your Number")
+    return num * num * num
+}
 
-// console.log(power())
+console.log(power())
 
 //............................Q17.......................................
 
-// function dice(){
-//     var diceValue = Math.ceil(Math.random() * 6)
-//     return diceValue
-// }
+function dice(){
+    var diceValue = Math.ceil(Math.random() * 6)
+    return diceValue
+}
 
-// console.log(dice())
+console.log(dice())
 
 //............................Q18.......................................
 
-// var x = 32243;
+var x = 32243;
 
-// function reverse(){
-//     x = x + ""
-//   return  x.split("").reverse().join("")
-// }
-// console.log(reverse())
+function reverse(){
+    x = x + ""
+  return  x.split("").reverse().join("")
+}
+console.log(reverse())
 //............................Q19.......................................
 
-// function palandrom(){
-//     var userInput = prompt("Enter Text")
-//     var reverseText = ''
+function palandrom(){
+    var userInput = prompt("Enter Text")
+    var reverseText = ''
    
-// for(var i =userInput.length -1; i >= 0; i--){
-//     reverseText += userInput[i]
-// }
-//    if(userInput == reverseText){
-//     console.log("Palandrom")
-//    }
-//    else{
-//     console.log("Not a palandrom")
-//    }
-// }
-// console.log(palandrom())
+for(var i =userInput.length -1; i >= 0; i--){
+    reverseText += userInput[i]
+}
+   if(userInput == reverseText){
+    console.log("Palandrom")
+   }
+   else{
+    console.log("Not a palandrom")
+   }
+}
+console.log(palandrom())
 
-//............................Q20.......................................
-
-
-//............................Q21.......................................
-//............................Q22.......................................
 
 
